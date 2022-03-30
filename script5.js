8685a4235abf3b065e86e290dc98e93333249101
JT921111 <72879314+JT921111@users.noreply.github.com>
2022-03-30 11:21:18 +0800

    Add files via upload

--- script5.js
+++ script5.js
@@ -5,3 +5,19 @@ $('.form').on('click', function () {
     $('.copied').removeClass('showup').addClass('hidden');
   }, 1200);
 })
+
+setInterval(function () {
+  //console.log(parseInt($('body').scrollTop() / $('body').height() * 100));
+  if (parseInt($('body').scrollTop() / $('body').height() * 100) < 5) {
+    $('a').removeClass('next');
+    $('a:nth-of-type(1)').addClass('next');
+  } else if (parseInt($('body').scrollTop() / $('body').height() * 100) < 37) {
+    $('a').removeClass('next');
+    $('a:nth-of-type(2)').addClass('next');
+  } else if (parseInt($('body').scrollTop() / $('body').height() * 100) < 55) {
+    $('a').removeClass('next');
+    $('a:nth-of-type(3)').addClass('next');
+  } else {
+    $('a').removeClass('next');
+  }
+}, 100);
