b5df3c173bf17f287d6378173e6c80faa40c288e
JT921111 <72879314+JT921111@users.noreply.github.com>
2022-03-30 21:06:03 +0800

    Add files via upload

--- script5.js
+++ script5.js
@@ -7,17 +7,22 @@ $('.form').on('click', function () {
 })
 
 setInterval(function () {
-  //console.log(parseInt($('body').scrollTop() / $('body').height() * 100));
   if (parseInt($('body').scrollTop() / $('body').height() * 100) < 5) {
     $('a').removeClass('next');
+    $('.scroller .scroll-tag:nth-of-type(2), .scroller .scroll-tag:nth-of-type(3), .scroller .scroll-tag:nth-of-type(4)').addClass('next-tag');
     $('a:nth-of-type(1)').addClass('next');
   } else if (parseInt($('body').scrollTop() / $('body').height() * 100) < 37) {
     $('a').removeClass('next');
+    $('.scroller .scroll-tag:nth-of-type(2)').removeClass('next-tag');
+    $('.scroller .scroll-tag:nth-of-type(3), .scroller .scroll-tag:nth-of-type(4)').addClass('next-tag');
     $('a:nth-of-type(2)').addClass('next');
   } else if (parseInt($('body').scrollTop() / $('body').height() * 100) < 55) {
     $('a').removeClass('next');
+    $('.scroller .scroll-tag:nth-of-type(2), .scroller .scroll-tag:nth-of-type(3)').removeClass('next-tag');
+    $('.scroller .scroll-tag:nth-of-type(4)').addClass('next-tag');
     $('a:nth-of-type(3)').addClass('next');
   } else {
     $('a').removeClass('next');
+    $('.scroller .scroll-tag:nth-of-type(2), .scroller .scroll-tag:nth-of-type(3), .scroller .scroll-tag:nth-of-type(4)').removeClass('next-tag');
   }
 }, 100);
