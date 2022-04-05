6d31d44147392ea19772d09e04603236185e6a53
JT921111 <72879314+JT921111@users.noreply.github.com>
2022-04-05 13:26:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -59,42 +59,42 @@ function sortTable(n) {
         }
       }
     }
-  } else {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("AllList");
-    switching = true;
-    dir = "asc";
-    while (switching) {
-      switching = false;
-      rows = table.rows;
-      for (i = 2; i < (rows.length - 1); i++) {
-        shouldSwitch = false;
-        x = rows[i].getElementsByTagName("td")[n];
-        y = rows[i + 1].getElementsByTagName("td")[n];
-        if (dir == "asc") {
-          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            shouldSwitch = true;
-            break;
-          }
-        } else if (dir == "desc") {
-          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            shouldSwitch = true;
-            break;
-          }
-        }
-      }
-      if (shouldSwitch) {
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        switchcount++;
-      } else {
-        if (switchcount == 0 && dir == "asc") {
-          dir = "desc";
-          switching = true;
-        }
-      }
-    }
-  }
+  }// else {
+    // var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    // table = document.getElementById("AllList");
+    // switching = true;
+    // dir = "asc";
+    // while (switching) {
+    //   switching = false;
+    //   rows = table.rows;
+    //   for (i = 2; i < (rows.length - 1); i++) {
+    //     shouldSwitch = false;
+    //     x = rows[i].getElementsByTagName("td")[n];
+    //     y = rows[i + 1].getElementsByTagName("td")[n];
+    //     if (dir == "asc") {
+    //       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+    //         shouldSwitch = true;
+    //         break;
+    //       }
+    //     } else if (dir == "desc") {
+    //       if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+    //         shouldSwitch = true;
+    //         break;
+    //       }
+    //     }
+    //   }
+    //   if (shouldSwitch) {
+    //     rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+    //     switching = true;
+    //     switchcount++;
+    //   } else {
+    //     if (switchcount == 0 && dir == "asc") {
+    //       dir = "desc";
+    //       switching = true;
+    //     }
+    //   }
+    // }
+  //}
 }
 
 const modefilter = [1, 1, 1, 1, 1];
